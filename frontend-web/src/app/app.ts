6c3562fe202f4dd9436f0b32e77d6baccbad932f
Daniel Sava <danielsavas@gmail.com>
2026-03-11 21:50:10 -0300

    PrimeNG instalação

--- frontend-web/src/app/app.ts
+++ frontend-web/src/app/app.ts
@@ -1,5 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PrimeNG } from 'primeng/config';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,23 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
+
   protected readonly title = signal('frontend-web');
+
+  constructor(private primeng: PrimeNG) { }
+
+  ngOnInit(): void {
+
+    this.primeng.ripple.set(true);
+
+    this.primeng.zIndex = {
+      modal: 1100,    // dialog, sidebar
+      overlay: 1000,  // dropdown, overlaypanel
+      menu: 1000,     // overlay menus
+      tooltip: 1100   // tooltip
+    };
+  }
+
+
 }
